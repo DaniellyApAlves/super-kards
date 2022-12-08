7fa878ef32146fabaa6fd4e6a1efe89928f203a4
DanyApAlves <danyaparecida.alves@gmail.com>
2022-12-08 18:59:29 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Cartões Interativos
+
+📌 Criação de uma página com cartões interativos de personagens.
+
+📝 No desafio proposto teria que ser feito uma página com cartões interativos, ao clicar no cartão o personagem e suas características de jogo aparecem, sendo elas:
+
+↩ Antes:
+Os cartões foram feitos com os personagens do Pokémon (nome titulado: pokedex), com fundo cinza padrão e botões na lateral para click.
+
+↪ Depois:
+Alteração dos personagens para o do Mario Kart (nome titulado: super kard), o fundo foi alterado para um dos levels do jogo.
+
+🖋 Caligrafias
+Caligrafia: 'Poppins', sans-serif
+
+© Imagens
+Google Images
+
+🛠 Ferramentas Utilizadas
+HTML 5
+CSS 3
+JAVASCRIPT 5
---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+const listaSelecaoKarts = document.querySelectorAll('.personagem')
+const marioKart = document.querySelectorAll('.kart-mario')
+
+listaSelecaoKarts.forEach(personagem => {
+    personagem.addEventListener('click', () => {
+    
+        const cartaoPersonagemAberto = document.querySelector('.aberto')
+        cartaoPersonagemAberto.classList.remove('aberto')
+    
+        const idPersonagemSelecionado = personagem.attributes.id.value
+
+        const idDoCartaoPersonagemParaAbrir = 'cartao-' + idPersonagemSelecionado
+
+        const cartaoPersonagemParaAbrir = document.getElementById(idDoCartaoPersonagemParaAbrir)
+
+        cartaoPersonagemParaAbrir.classList.add('aberto')
+
+        const personagemAtivoNaListagem = document.querySelector('.ativo')
+        personagemAtivoNaListagem.classList.remove('ativo')
+
+        const personagemSelecionadoNaListagem = document.getElementById(idPersonagemSelecionado)
+        personagemSelecionadoNaListagem.classList.add('ativo')
+    })
+})
